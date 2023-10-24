8fbe82acbc6c5e46ac06e5e98c09c3b64a82f38b
Bohan <zhangbohan1993@gmail.com>
2023-10-24 16:40:23 -0700

    skeletal increment demo version
    enhanced readability, separate Javascript from index.html to main.js

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,22 @@
 from flask import Flask, render_template, request, jsonify, session
-from flask_socketio import SocketIO, emit, join_room, leave_room
+from flask_socketio import SocketIO, emit
 
+# Constants
 MAX_PLAYERS_PER_LOBBY = 6
 MIN_PLAYERS_PER_LOBBY = 3
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'secret!'
-    socketio = SocketIO(app)
+    socketio = SocketIO(app, manage_session=False)
 
-    users_db = {}  # This will store user data as {username: password}
-    lobbies = {}  # This will store lobbies with structure {lobby_id: [list_of_players]}
+    # In-memory databases
+    users_db = {}  # {username: password}
+    lobbies = {}   # {lobby_id: [list_of_players]}
+
+    # -----------------
+    # Web Routes
+    # -----------------
 
     @app.route('/')
     def index():
@@ -22,30 +28,22 @@ def create_app():
         password = request.form.get('password')
         if username in users_db:
             return 'Username already taken!', 400
-        else:
-            users_db[username] = password
-            return 'User registered successfully!'
+        users_db[username] = password
+        return 'User registered successfully!'
 
     @app.route('/login', methods=['POST'])
     def login():
         username = request.form.get('username')
         password = request.form.get('password')
         if users_db.get(username) == password:
-            session['username'] = username  # Save username in the session
+            session['username'] = username
             return 'Login successful!'
-        else:
-            return 'Invalid username or password!', 401
-
-    @socketio.on('send_message')
-    def handle_message(message):
-        print('Received message:', message)
-        emit('broadcast_message', message, broadcast=True)
+        return 'Invalid username or password!', 401
 
     @app.route('/create_lobby', methods=['POST'])
     def create_lobby():
         lobby_id = len(lobbies) + 1
         lobbies[lobby_id] = [session['username']]
-        print("Emitting lobby_update from create_lobby")
         socketio.emit('lobby_update', lobbies)
         return jsonify({"lobby_id": lobby_id, "players": lobbies[lobby_id]})
 
@@ -54,26 +52,34 @@ def create_app():
         if lobby_id not in lobbies:
             return "Lobby not found!", 404
 
-        # Check if the lobby is full
         if len(lobbies[lobby_id]) >= MAX_PLAYERS_PER_LOBBY:
             return "Lobby is full!", 403
 
         if session['username'] not in lobbies[lobby_id]:
             lobbies[lobby_id].append(session['username'])
-            print("Emitting lobby_update from join_lobby")
             socketio.emit('lobby_update', lobbies) 
-        return jsonify({"lobby_id": lobby_id, "players": lobbies[lobby_id]})
 
+        return jsonify({"lobby_id": lobby_id, "players": lobbies[lobby_id]})
 
     @app.route('/lobbies', methods=['GET'])
     def get_lobbies():
         return jsonify(lobbies)
+
+    # -----------------
+    # Socket Events
+    # -----------------
+
+    @socketio.on('send_message')
+    def handle_message(data):
+        username = data.get('username', 'Anonymous')
+        message = data['message']
+        formatted_message = f"{username}: {message}"
+        emit('broadcast_message', formatted_message, broadcast=True)
 
     return app
 
 app = create_app()
 
 if __name__ == '__main__':
-    from flask_socketio import SocketIO
     socketio = SocketIO(app)
-    socketio.run(app, debug=True)  # type: ignore
+    socketio.run(app, debug=True) # type: ignore
